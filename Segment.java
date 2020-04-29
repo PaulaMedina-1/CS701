c313744df3d46b1fda8c2ca8b4a16d644cf83796
PaulaMedina-1 <55564623+PaulaMedina-1@users.noreply.github.com>
2020-04-29 13:05:07 +0200

    Update Segment.java

--- Segment.java
+++ Segment.java
@@ -1,6 +1,7 @@
 public class Segment {
     public Point p1;
     public Point p2;
+    public int value;
 
 
     public Segment(Point p1, Point p2) {
@@ -8,6 +9,7 @@ public class Segment {
         this.p1.parent = this;
         this.p2 = p2;
         this.p2.parent = this;
+        this.calculate_value(this.startPoint().getx());
         if (p1.getx() > p2.getx()) {
             this.p1 = p2;
             this.p2 = p1;
@@ -60,14 +62,13 @@ public class Segment {
 
     }
     
-    public void calculate_value(Segment s) {
-        double x1 = s.startPoint().getx();
-        double x2 = s.endPoint().getx();
-        double y1 = s.startPoint().gety();
-        double y2 = s.endPoint().gety();
-        int Nvalue = (int) (y1 + (((y2 - y1) / (x2 - x1)) * (value - x1)));
+     public void calculate_value(int value) {
+        double x1 = this.startPoint().getx();
+        double x2 = this.endPoint().getx();
+        double y1 = this.startPoint().gety();
+        double y2 = this.endPoint().gety();
+        this.value = (int) (y1 + (((y2 - y1) / (x2 - x1)) * (value - x1)));
     }
-
     public void set_value(int value) {
         this.value = value;
     }
