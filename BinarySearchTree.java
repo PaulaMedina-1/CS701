0ba8d5478a4886487ebd477968a3021f8a6944d1
danielberekdar <42421210+DanielBerekdar@users.noreply.github.com>
2020-05-03 11:34:49 -0400

    Modified Point.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,262 +1,218 @@
+public class BinarySearchTree<AnyType extends Comparable<? super AnyType>> {
+    /**
+     * Construct the tree.
+     */
+    public BinarySearchTree() {
+        root = null;
+    }
 
-public class BinarySearchTree<AnyType extends Comparable<? super AnyType>>
-{
-	/**
-	 * Construct the tree.
-	 */
-	public BinarySearchTree( )
-	{
-		root = null;
-	}
-
-	/**
-	 * Insert into the tree; duplicates are ignored.
-	 * @param x the item to insert.
-	 */
-
-	//ATTENTION: BOOLEAN VALUE IS NECESSARY TO OVERLOAD THE METHOD INSERT
-	public void insert(AnyType x, boolean value) 
-	{	// creating a binary node with item.element = x
-		BinaryNode<AnyType> item = new BinaryNode<AnyType>(x);
-		// check if our tree is empty
-		if (root == null) {
-			root = item;
-			return;
-		}
-		BinaryNode<AnyType> parent = null;
-		BinaryNode<AnyType> current = root;
-		
-		while (current != null) {
-			parent = current;
-			if (current.element.compareTo(x) <= 0) {
-				current = current.right;
-			}
-			else {
-				current = current.left;
-			}
-		}
-		
-		if (parent.element.compareTo(x) <= 0 ) {
-			parent.right = item;
-		}
-		else {
-			parent.left = item;
-		}
-		return;
-	}
-	
-	public boolean contains(AnyType x, boolean guess) {
-		
-		BinaryNode<AnyType> item = new BinaryNode<AnyType>(x);
-		
-		if ( root == null) {
-			return false;
-		}
-		
-		BinaryNode<AnyType> current = root;
-		while(current != null) {
-			if (current.element.compareTo(x) >= 1) {
-				if (current.left != null && current.left.element == item) {
-					return true;
-				}
-			
-				current = current.left;
-			}
-			else {
-				
-				if (current.right != null && current.right.element == item) {
-					return true;
-				}
-				current = current.right;
-			}
-		}
-		// BinaryNode is not found if we went through the tree and got to this line
-		return false;
-	}
-
-	public void insert( AnyType x )
-	{
-		root = insert( x, root );
-	}
-
-	
-	public void remove( AnyType x )
-	{
-		root = remove( x, root );
-	}
-
-	
-	public AnyType findMin( )
-	{
-		if( isEmpty( ) )
-			throw new UnderFlowException( );
-		return findMin( root ).element;
-	}
-
-	
-	public AnyType findMax( )
-	{
-		if( isEmpty( ) )
-			throw new UnderFlowException( );
-		return findMax( root ).element;
-	}
-
-	
-	public boolean contains( AnyType x )
-	{
-		return contains( x, root );
-	}
-
-	
-	public void makeEmpty( )
-	{
-		root = null;
-	}
-
-	
-	public boolean isEmpty( )
-	{
-		return root == null;
-	}
-
-	
-	public void printTree( )
-	{
-		if( isEmpty( ) )
-			System.out.println( "Empty tree" );
-		else
-			printTree( root );
-	}
-
-	
-	 
-	private BinaryNode<AnyType> insert( AnyType x, BinaryNode<AnyType> t )
-	{
-		if( t == null )
-			return new BinaryNode<AnyType>( x, null, null );
-
-		int compareResult = x.compareTo( t.element );
-
-		if( compareResult < 0 )
-			t.left = insert( x, t.left );
-		else if( compareResult > 0 )
-			t.right = insert( x, t.right );
-		else
-			;  // Duplicate; do nothing
-		return t;
-	}
-
-	
-	private BinaryNode<AnyType> remove( AnyType x, BinaryNode<AnyType> t )
-	{
-		if( t == null )
-			return t;   // Item not found; do nothing
-
-		int compareResult = x.compareTo( t.element );
-
-		if( compareResult < 0 )
-			t.left = remove( x, t.left );
-		else if( compareResult > 0 )
-			t.right = remove( x, t.right );
-		else if( t.left != null && t.right != null ) // Two children
-		{
-			t.element = findMin( t.right ).element;
-			t.right = remove( t.element, t.right );
-		}
-		else
-			t = ( t.left != null ) ? t.left : t.right;
-		return t;
-	}
-
-	
-	private BinaryNode<AnyType> findMin( BinaryNode<AnyType> t )
-	{
-		if( t == null )
-			return null;
-		else if( t.left == null )
-			return t;
-		return findMin( t.left );
-	}
-
-	
-	private BinaryNode<AnyType> findMax( BinaryNode<AnyType> t )
-	{
-		if( t != null )
-			while( t.right != null )
-				t = t.right;
-
-		return t;
-	}
-
-	
-	private boolean contains( AnyType x, BinaryNode<AnyType> t )
-	{
-		if( t == null )
-			return false;
-
-		int compareResult = x.compareTo( t.element );
-
-		if( compareResult < 0 )
-			return contains( x, t.left );
-		else if( compareResult > 0 )
-			return contains( x, t.right );
-		else
-			return true;    // Match
-	}
-
-	
-	private void printTree( BinaryNode<AnyType> t )
-	{
-		if( t != null )
-		{
-			printTree( t.left );
-			System.out.println( t.element );
-			printTree( t.right );
-		}
-	}
-
-	
-	private int height( BinaryNode<AnyType> t )
-	{
-		if( t == null )
-			return -1;
-		else
-			return 1 + Math.max( height( t.left ), height( t.right ) );    
-	}
-
-	
-	private static class BinaryNode<AnyType>
-	{
-		
-		BinaryNode( AnyType theElement )
-		{
-			this( theElement, null, null );
-		}
-
-		BinaryNode( AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt )
-		{
-			element  = theElement;
-			left     = lt;
-			right    = rt;
-		}
-
-		AnyType element;            // The data in the node
-		BinaryNode<AnyType> left;   // Left child
-		BinaryNode<AnyType> right;  // Right child
-	}
-	static void seq(int low, int high, BinarySearchTree T) {
-		if (low<= high) {
-			int mid = (int)(low+high)/2;
-			T.insert(mid);
-			seq(low,mid-1,T);
-			seq(mid+1,high,T);
-		}
+    static void seq(int low, int high, BinarySearchTree T) {
+        if (low <= high) {
+            int mid = (int) (low + high) / 2;
+            T.insert(mid);
+            seq(low, mid - 1, T);
+            seq(mid + 1, high, T);
+        }
 
 
-	/** The tree root. */
-	private BinaryNode<AnyType> root;
+        /** The tree root. */
+        private BinaryNode<AnyType> root;
 
 
+    }
 
-	
+    /**
+     * Insert into the tree; duplicates are ignored.
+     *
+     * @param x the item to insert.
+     */
+
+    //ATTENTION: BOOLEAN VALUE IS NECESSARY TO OVERLOAD THE METHOD INSERT
+    public void insert(AnyType x, boolean value) {    // creating a binary node with item.element = x
+        BinaryNode<AnyType> item = new BinaryNode<AnyType>(x);
+        // check if our tree is empty
+        if (root == null) {
+            root = item;
+            return;
+        }
+        BinaryNode<AnyType> parent = null;
+        BinaryNode<AnyType> current = root;
+
+        while (current != null) {
+            parent = current;
+            if (current.element.compareTo(x) <= 0) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+
+        if (parent.element.compareTo(x) <= 0) {
+            parent.right = item;
+        } else {
+            parent.left = item;
+        }
+        return;
+    }
+
+    public boolean contains(AnyType x, boolean guess) {
+
+        BinaryNode<AnyType> item = new BinaryNode<AnyType>(x);
+
+        if (root == null) {
+            return false;
+        }
+
+        BinaryNode<AnyType> current = root;
+        while (current != null) {
+            if (current.element.compareTo(x) >= 1) {
+                if (current.left != null && current.left.element == item) {
+                    return true;
+                }
+
+                current = current.left;
+            } else {
+
+                if (current.right != null && current.right.element == item) {
+                    return true;
+                }
+                current = current.right;
+            }
+        }
+        // BinaryNode is not found if we went through the tree and got to this line
+        return false;
+    }
+
+    public void insert(AnyType x) {
+        root = insert(x, root);
+    }
+
+    public void remove(AnyType x) {
+        root = remove(x, root);
+    }
+
+    public AnyType findMin() {
+        if (isEmpty())
+            throw new UnderFlowException();
+        return findMin(root).element;
+    }
+
+    public AnyType findMax() {
+        if (isEmpty())
+            throw new UnderFlowException();
+        return findMax(root).element;
+    }
+
+    public boolean contains(AnyType x) {
+        return contains(x, root);
+    }
+
+    public void makeEmpty() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void printTree() {
+        if (isEmpty())
+            System.out.println("Empty tree");
+        else
+            printTree(root);
+    }
+
+    private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null)
+            return new BinaryNode<AnyType>(x, null, null);
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            t.left = insert(x, t.left);
+        else if (compareResult > 0)
+            t.right = insert(x, t.right);
+        else
+            ;  // Duplicate; do nothing
+        return t;
+    }
+
+    private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null)
+            return t;   // Item not found; do nothing
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            t.left = remove(x, t.left);
+        else if (compareResult > 0)
+            t.right = remove(x, t.right);
+        else if (t.left != null && t.right != null) // Two children
+        {
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        } else
+            t = (t.left != null) ? t.left : t.right;
+        return t;
+    }
+
+    private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t) {
+        if (t == null)
+            return null;
+        else if (t.left == null)
+            return t;
+        return findMin(t.left);
+    }
+
+    private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t) {
+        if (t != null)
+            while (t.right != null)
+                t = t.right;
+
+        return t;
+    }
+
+    private boolean contains(AnyType x, BinaryNode<AnyType> t) {
+        if (t == null)
+            return false;
+
+        int compareResult = x.compareTo(t.element);
+
+        if (compareResult < 0)
+            return contains(x, t.left);
+        else if (compareResult > 0)
+            return contains(x, t.right);
+        else
+            return true;    // Match
+    }
+
+    private void printTree(BinaryNode<AnyType> t) {
+        if (t != null) {
+            printTree(t.left);
+            System.out.println(t.element);
+            printTree(t.right);
+        }
+    }
+
+    private int height(BinaryNode<AnyType> t) {
+        if (t == null)
+            return -1;
+        else
+            return 1 + Math.max(height(t.left), height(t.right));
+    }
+
+    private static class BinaryNode<AnyType> {
+
+        AnyType element;            // The data in the node
+        BinaryNode<AnyType> left;   // Left child
+        BinaryNode<AnyType> right;  // Right child
+        BinaryNode(AnyType theElement) {
+            this(theElement, null, null);
+        }
+        BinaryNode(AnyType theElement, BinaryNode<AnyType> lt, BinaryNode<AnyType> rt) {
+            element = theElement;
+            left = lt;
+            right = rt;
+        }
+    }
 }
--- Point.java
+++ Point.java
@@ -1,39 +1,111 @@
+public class Point implements Comparable<Point> {
+    public Segment parent;
+    public int xcoord;
+    public int ycoord;
 
-public class Point implements Comparable<Point>{
-	public Segment parent;
-	public int xcoord;
-	public int ycoord;
-	
-	//Creating a point
-	public Point(int x, int y) {
-		this.xcoord = x;
-		this.ycoord = y;
-	}
-	public int compareTo(Point p){
-	    if(this.getx() == p.gety()){
-	        return ((Integer)this.gety()).compareTo(p.gety());
-        }else{
-            return ((Integer)this.getx()).compareTo(p.getx());
+    //Creating a point
+    public Point(int x, int y) {
+        this.xcoord = x;
+        this.ycoord = y;
+    }
+
+    public int compareTo(Point p) {
+        if (this.getx() == p.gety()) {
+            return ((Integer) this.gety()).compareTo(p.gety());
+        } else {
+            return ((Integer) this.getx()).compareTo(p.getx());
         }
     }
-	
-	//methods to get x and y coordinates from a point
-	
-	public int getx() {
-		return this.xcoord;
-		
-	}
-	public int gety() {
-		return this.ycoord;
-	}
-	
-	public void set_x(int new_x) {
-	    this.xcoord = new_x;
-	  }
-	  
-	  public void set_y(int new_y) {
-	    this.ycoord = new_y;
-	  }
+
+    //methods to get x and y coordinates from a point
+
+    public int getx() {
+        return this.xcoord;
+
+    }
+
+    public int gety() {
+        return this.ycoord;
+    }
+
+    public void set_x(int new_x) {
+        this.xcoord = new_x;
+    }
+
+    public void set_y(int new_y) {
+        this.ycoord = new_y;
+    }
 
 
+    // PROBLEM 2
+    public int findRange(Point[] Points) {
+        int xMax = Integer.MIN_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        int xMin = Integer.MAX_VALUE;
+        int yMin = Integer.MAX_VALUE;
+
+        for (Point point : Points) {
+            if (point.getx() > xMax) {
+                xMax = point.getx();
+            }
+            if (point.gety() > yMax) {
+                yMax = point.gety();
+            }
+            if (point.getx() < xMin) {
+                xMin = point.getx();
+            }
+            if (point.gety() < yMin) {
+                yMin = point.gety();
+            }
+        }
+        return Math.max(xMax - xMin, yMax - yMin);
+    }
+
+    public Point subtract(Point point) {
+        return new Point(getx() - point.getx(), gety() - point.gety());
+    }
+
+    public boolean sameSegment(Point pi, Point pj, Point pk) {
+
+        if (Math.min(pi.getx(), pj.getx()) <= pk.getx()
+                && Math.max(pi.getx(), pj.getx()) >= pk.getx()
+                && Math.min(pi.gety(), pj.gety()) <= pk.gety()
+                && Math.max(pi.gety(), pj.gety()) >= pk.gety()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int product(Point p1, Point p2) {
+        return p1.getx() * p2.gety() - p2.getx() * p1.gety();
+    }
+
+    public int direction(Point pi, Point pj, Point pk) {
+        return product(pk.subtract(pi), pj.subtract(pi));
+    }
+
+
+    public boolean segmentsIntereesct(Point p1, Point p2, Point p3, Point p4) {
+        int d1 = direction(p3, p4, p1);
+        int d2 = direction(p3, p4, p2);
+        int d3 = direction(p1, p2, p3);
+        int d4 = direction(p1, p2, p4);
+
+        if (((d1 > 0F && d2 < 0)
+                || (d1 < 0 && d2 > 0))
+                && ((d3 > 0 && d4 < 0)
+                || (d3 < 0 && d4 > 0))
+        ) {
+            return true;
+        } else if (Math.abs(d1) == 0 && sameSegment(p3, p4, p1)) {
+            return true;
+        } else if (Math.abs(d2) == 0 && sameSegment(p3, p4, p2)) {
+            return true;
+        } else if (Math.abs(d3) == 0 && sameSegment(p1, p2, p3)) {
+            return true;
+        } else if (Math.abs(d4) == 0 && sameSegment(p1, p2, p4)) {
+            return true;
+        }
+        return false;
+    }
 }
